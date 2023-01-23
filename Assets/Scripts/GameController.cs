f62c7e305b642985a645924a9deb4cc126f6223e
Vadim <iskolen@mail.ru>
2023-01-23 20:16:39 +0300

    Added basic objects, cube building and losing mechanics

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private CubePosition nowCube = new CubePosition(0, 1, 0);
+    public float cubeChangePlaceSpeed = 1f;
+    public Transform cubeToPlace;
+    public GameObject cubeToCreate, allCubes;
+    private Rigidbody allCubesRb;
+    private bool isLose;
+    private Coroutine showCubePlace;
+
+    private List<Vector3> allCubesPositions = new List<Vector3>
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(0, 1, 0),
+        new Vector3(1, 0, 0),
+        new Vector3(-1, 0, 0),
+        new Vector3(0, 0, 1),
+        new Vector3(0, 0, -1),
+        new Vector3(1, 0, 1),
+        new Vector3(-1, 0, -1),
+        new Vector3(-1, 0, 1),
+        new Vector3(1, 0, -1)
+    };
+
+    private void Start()
+    {
+        allCubesRb = allCubes.GetComponent<Rigidbody>();
+        showCubePlace = StartCoroutine(ShowCubePlace());
+    }
+
+    private void Update()
+    {
+        if((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null)
+        {
+#if !UNITY_EDITOR
+                if(Input.GetTouch(0).phase != TouchPhase.Began)
+                    return;
+#endif
+            GameObject newCube = Instantiate(
+                cubeToCreate,
+                cubeToPlace.position,
+                Quaternion.identity);
+            newCube.transform.SetParent(allCubes.transform);
+            nowCube.setVector(cubeToPlace.position);
+            allCubesPositions.Add(nowCube.getVector());
+
+            allCubesRb.isKinematic = true;
+            allCubesRb.isKinematic = false;
+            SpawnPositions();
+        }
+
+        if(!isLose && allCubesRb.velocity.magnitude > 0.1f)
+        {
+            Destroy(cubeToPlace.gameObject);
+            isLose = true;
+            StopCoroutine(showCubePlace);
+        }
+    }
+
+    IEnumerator ShowCubePlace()
+    {
+        while(true)
+        {
+            SpawnPositions();
+            yield return new WaitForSeconds(cubeChangePlaceSpeed);
+        }    
+    }
+
+    [Obsolete]
+    private void SpawnPositions()
+    {
+        List<Vector3> positions = new List<Vector3>();
+        if (IsPositionEmpty(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z))
+            && nowCube.x + 1 != cubeToPlace.position.x)
+            positions.Add(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z));
+        if (IsPositionEmpty(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z))
+            && nowCube.x - 1 != cubeToPlace.position.x)
+            positions.Add(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z));
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z))
+            && nowCube.y + 1 != cubeToPlace.position.y)
+            positions.Add(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z));
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z))
+            && nowCube.y - 1 != cubeToPlace.position.y)
+            positions.Add(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z));
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1))
+            && nowCube.z + 1 != cubeToPlace.position.z)
+            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1));
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1))
+            && nowCube.z - 1 != cubeToPlace.position.z)
+            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1));
+
+        cubeToPlace.position = positions[UnityEngine.Random.RandomRange(0, positions.Count)];
+    }
+
+    private bool IsPositionEmpty(Vector3 targetPosition)
+    {
+        if(targetPosition.y == 0)
+            return false;
+
+        foreach(Vector3 position in allCubesPositions)
+        {
+            if (position.x == targetPosition.x && position.y == targetPosition.y && position.z == targetPosition.z)
+                return false;
+        }
+        return true;
+    }
+}
+
+struct CubePosition
+{
+    public int x, y, z;
+
+    public CubePosition(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3 getVector()
+    {
+        return new Vector3(x, y, z);
+    }
+
+    public void setVector(Vector3 position)
+    {
+        x = Convert.ToInt32(position.x);
+        y = Convert.ToInt32(position.y);
+        z = Convert.ToInt32(position.z);
+    }
+}
